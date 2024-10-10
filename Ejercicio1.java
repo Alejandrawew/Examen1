5ae5ace710ca035f83f75803149bda44c6b109c9
A611\1DAW <alejandrawew@gmail.com>
2024-10-10 20:42:26 +0200

    Ejercicios de frases, posiciones, contar letras...

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
+
 public class Ejercicio1 {
 
     public void Examen_ejercicio1 () {
 
 
-
+        Scanner valorabsoluto = new Scanner(System.in);
 
 
 
--- Main.java
+++ Main.java
@@ -3,8 +3,13 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Ejercicio2 Examen_ejercicio2 = new Ejercicio2();
-        Examen_ejercicio2.Examen_ejercicio2();
+       // Ejercicio2 Examen_ejercicio2 = new Ejercicio2();
+        //Examen_ejercicio2.Examen_ejercicio2();//
+
+        CosasString cosasString = new CosasString();
+        cosasString.caracteres();
+
+
 
     }
 
